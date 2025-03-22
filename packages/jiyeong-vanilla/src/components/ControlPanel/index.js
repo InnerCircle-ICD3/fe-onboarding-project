e8298e21b08de73c40070ea1db10d04a1af52dc0
gzero1728 <gzero1728@gmail.com>
2025-03-22 18:49:18 +0900

    [FEATURE] : 히스토리 목록 태그 사용

--- packages/jiyeong-vanilla/src/components/ControlPanel/index.js
+++ packages/jiyeong-vanilla/src/components/ControlPanel/index.js
@@ -4,15 +4,15 @@ export const ControlPanel = () => {
       <div class="control-panel-top">
         <div class="input-display">3,000</div>
         <div class="control-buttons">
-        <button class="control-btn">투입</button>
-        <button class="control-btn">반환</button>
+          <button class="control-btn">투입</button>
+          <button class="control-btn">반환</button>
+        </div>
       </div>
-      </div>
-      <div class="transaction-history">
-        <p>2000원을 투입했습니다.</p>
-        <p>오렌지쥬스를 구매했습니다.</p>
-        <p>200원을 반환합니다.</p>
-      </div>
+      <ol class="transaction-history">
+        <li>2000원을 투입했습니다.</li>
+        <li>오렌지쥬스를 구매했습니다.</li>
+        <li>200원을 반환합니다.</li>
+      </ol>
     </div>
   `;
 };
