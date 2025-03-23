6b869cc9a0ff9e935321520b9deece23ea1ab19b
unknown <tmdgml11342@gmail.com>
2025-03-23 12:42:29 +0900

    chore: 불필요한 코드 삭제

--- packages/seunghee-vanilla/src/main.js
+++ packages/seunghee-vanilla/src/main.js
@@ -1,24 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const mds = [
+  { name: "콜라", price: 1500 },
+  { name: "사이다", price: 1700 },
+  { name: "환타", price: 2000 },
+  { name: "포카리", price: 1300 },
+  { name: "게토레이", price: 1800 },
+  { name: "밀키스", price: 1600 },
+  { name: "레쓰비", price: 1900 },
+  { name: "코카콜라", price: 2500 },
+  { name: "펩시", price: 2500 },
+  { name: "마운틴듀", price: 2500 },
+  { name: "스프라이트", price: 2500 },
+  { name: "토레타", price: 2500 },
+  { name: "페리에", price: 2500 },
+  { name: "맥콜", price: 2500 },
+];
 
-setupCounter(document.querySelector('#counter'))
+function renderMdButtons() {
+  const container = document.getElementById("md-container");
+  container.innerHTML = "";
+
+  mds.forEach((md) => {
+    const button = document.createElement("button");
+    button.textContent = `${md.name} - ${md.price}원`;
+
+    container.appendChild(button);
+  });
+}
+
+renderMdButtons();
