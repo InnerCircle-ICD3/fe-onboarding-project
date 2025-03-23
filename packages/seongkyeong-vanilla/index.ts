ef50a6357a7d5736c498be1e1ef98929e4f35bb2
ParkBible <p8yft8903@gmail.com>
2025-03-24 00:21:46 +0900

    feat: 버튼 상수로 관리 및 hover, click style 지정
    - logging box는 div -> textarea로 변경하였습니다.

---
+++ packages/seongkyeong-vanilla/index.ts
@@ -0,0 +1,38 @@
+interface Menu {
+    name: string;
+    price: number;
+}
+
+const menuItems: Menu[] = [
+    { name: "콜라", price: 1500 },
+    { name: "속이사이다", price: 1700 },
+    { name: "판타지판타", price: 1500 },
+    { name: "오뎅국물", price: 1800 },
+    { name: "부장라떼", price: 800 },
+    { name: "판타지판타", price: 1500 },
+    { name: "레드뿔", price: 2500 },
+    { name: "핫세븐", price: 1900 },
+    { name: "커피우유", price: 1400 }
+];
+
+const drawMenuButtons = () => {
+    const $menuWrapper = document.querySelector(".menu-btn-wrapper");
+
+    menuItems.forEach(menu => {
+        const $menuBtn = document.createElement("button");
+        const $name = document.createElement("p");
+        const $price = document.createElement("p");
+        
+        $menuBtn.className = "menu-item";
+        $name.innerHTML = menu.name;
+        $price.innerHTML = `${menu.price}원`;
+        $menuBtn.appendChild($name);
+        $menuBtn.appendChild($price);
+
+        $menuWrapper?.appendChild($menuBtn);
+    });
+}
+
+window.addEventListener("load", () => {
+    drawMenuButtons();
+});
