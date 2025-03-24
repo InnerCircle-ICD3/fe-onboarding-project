e69c855dc39c7f68bae21a4561432a7a2ae092fb
BORA <a52447879@gmail.com>
2025-03-24 21:20:00 +0900

    refactor: 상품 렌더링 리팩토링

--- packages/bora-vanilla/src/main.js
+++ packages/bora-vanilla/src/main.js
@@ -9,42 +9,59 @@ const insertForm = document.querySelector(
   '.vending-machine-insert-form'
 );
 const priceInput = document.querySelector('.price-input');
+
 const logContainer = document.querySelector(
   '.log-message-container > div'
 );
 
-// 버튼 렌더링
+// 상품 렌더링
 const renderProducts = (productsData) => {
-  const productButtons = productsData
-    .map((product) => {
-      return `
-      <button
-        class="product-button relative flex flex-col items-center justify-center p-4 bg-white border-2 border-gray-200"
-        data-id="${product.id}"
-        data-price="${product.price}"
-        data-name="${product.name}"
-      >
-        <div class="product-name font-semibold text-gray-800">${product.name}</div>
-        <div class="product-price text-blue-600 font-bold">${product.price}원</div>
-      </button>
-    `;
-    })
-    .join('');
+  console.log(productsData, 'productsData');
 
-  buttonContainer.innerHTML = productButtons;
+  const fragment = document.createDocumentFragment();
+
+  for (const product of productsData) {
+    const button = createProductButton(product);
+    fragment.appendChild(button);
+  }
+
+  buttonContainer.appendChild(fragment);
+};
+
+const createProductButton = (product) => {
+  const button = document.createElement('button');
+  button.className =
+    'product-button relative flex flex-col items-center justify-center p-4 bg-white border-2 border-gray-200';
+
+  button.dataset.id = product.id;
+
+  // 상품명
+  const name = document.createElement('div');
+  name.className = 'product-name font-semibold text-gray-800';
+  name.textContent = product.name;
+
+  // 가격
+  const price = document.createElement('div');
+  price.className = 'product-price text-blue-600 font-bold';
+  price.textContent = `${product.price}원`;
+
+  button.appendChild(name);
+  button.appendChild(price);
+
+  return button;
 };
 
 const renderLog = (message) => {
   const logItem = document.createElement('div');
   logItem.className = 'p-1 border-gray-200 text-sm';
   logItem.textContent = `${message}`;
-  logContainer.appendChild(logItem);
+  logContainer?.appendChild(logItem);
 };
 
 // 금액 입력
 insertForm?.addEventListener('submit', (e) => {
   e.preventDefault();
-  const amount = parseInt(priceInput.value);
+  const amount = Number.parseInt(priceInput.value);
 
   // 양수만 가능
   if (isNaN(amount) || amount <= 0) {
@@ -59,8 +76,8 @@ insertForm?.addEventListener('submit', (e) => {
 });
 
 const init = () => {
-  const productsData = productService.getAllProducts();
-  renderProducts(productsData);
+  const buttonData = productService.getAllProducts();
+  renderProducts(buttonData);
 };
 
 init();
