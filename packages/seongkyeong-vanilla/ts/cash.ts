8e6c0478dfcd2c2cb63ccf1602afc8a0bcdc6c44
ParkBible <p8yft8903@gmail.com>
2025-03-25 22:20:40 +0900

    refactor: HTML element 변수에 제네릭 타입 사용, valueAsNumber 로 input[type=number] 값 가져오도록 변경

--- packages/seongkyeong-vanilla/ts/cash.ts
+++ packages/seongkyeong-vanilla/ts/cash.ts
@@ -1,49 +1,51 @@
-const $cashInput = document.querySelector(".cash") as HTMLInputElement;
-const $balanceInput = document.querySelector(".balance") as HTMLInputElement;
-const $putButton = document.querySelector(".put-btn") as HTMLButtonElement;
-const $returnButton = document.querySelector(".rtn-btn") as HTMLButtonElement;
-const $warningContent = document.querySelector(".warning-box") as HTMLDivElement;
+const $cashInput = document.querySelector<HTMLInputElement>("input[type=number].cash");
+const $balanceInput = document.querySelector<HTMLInputElement>("input[type=number].balance");
+const $putButton = document.querySelector<HTMLButtonElement>(".put-btn");
+const $returnButton = document.querySelector<HTMLButtonElement>(".rtn-btn");
+const $warningContent = document.querySelector<HTMLDivElement>(".warning-box");
 
-$putButton.addEventListener("click", () => {
-    const cash = Number($cashInput.value);
-
-    if (cash > 0) {
-        putCash(cash);
-        clearWarning();
-    } else {
-        setWarning("투입 금액은 0 이하일 수 없습니다.");
-    }
-});
-
-$returnButton.addEventListener("click", () => {
-    const balance = Number($balanceInput.value);
-
-    if (balance > 0) {
-        returnCash(balance);
-        clearWarning();
-    } else {
-        setWarning("반환할 금액이 없습니다.");
-    }
-});
-
-const putCash = (cash: number) => {
-    let balance = Number($balanceInput.value);
-
-    $cashInput.value = "";
-    balance += cash;
+if ($cashInput && $balanceInput && $putButton && $returnButton && $warningContent) {
+    $putButton.addEventListener("click", () => {
+        const cash = $cashInput.valueAsNumber;
     
-    $balanceInput.value = balance.toString();
-}
-
-const returnCash = (balance: number) => {
-    $cashInput.value = balance.toString();
-    $balanceInput.value = "0";
-}
-
-const setWarning = (message: string) => {
-    $warningContent.innerText = message;
-}
-
-const clearWarning = () => {
-    $warningContent.innerText = "";
+        if (cash > 0) {
+            putCash(cash);
+            clearWarning();
+        } else {
+            setWarning("투입 금액은 0 이하일 수 없습니다.");
+        }
+    });
+    
+    $returnButton.addEventListener("click", () => {
+        const balance = $balanceInput.valueAsNumber;
+    
+        if (balance > 0) {
+            returnCash(balance);
+            clearWarning();
+        } else {
+            setWarning("반환할 금액이 없습니다.");
+        }
+    });
+    
+    const putCash = (cash: number) => {
+        let balance = $balanceInput.valueAsNumber;
+    
+        $cashInput.value = "";
+        balance += cash;
+        
+        $balanceInput.value = balance.toString();
+    }
+    
+    const returnCash = (balance: number) => {
+        $cashInput.value = balance.toString();
+        $balanceInput.value = "0";
+    }
+    
+    const setWarning = (message: string) => {
+        $warningContent.innerText = message;
+    }
+    
+    const clearWarning = () => {
+        $warningContent.innerText = "";
+    }
 }
