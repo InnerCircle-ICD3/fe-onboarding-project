58f8f2b2b688653d4466a9e1f8a3abc169315ae1
SeoJaeWan <sjw7324@naver.com>
2025-03-25 21:19:29 +0900

    feat: payment 이벤트 위임

--- packages/seojaewan-vanilla/src/js/payment.ts
+++ packages/seojaewan-vanilla/src/js/payment.ts
@@ -1,8 +1,16 @@
 
+import checkInstance from "./checkType";
 import { numberWithComma } from "./commas";
 import { totalPrice } from "./main";
 
-const handlePayment = (value: string) => {
+const handlePayment = (e: MouseEvent) => {
+    const ul = document.querySelector(".button--list");
+    if(!checkInstance(ul, HTMLUListElement) || !checkInstance(e.target, HTMLElement)) return;
+    
+    const button = e.target.closest('button.button--payment');
+    if(!checkInstance(button, HTMLButtonElement) || !ul.contains(button)) return;
+
+    const value = button.value;
     const currentValue = Number(value);
 
     const totalPriceElement = document.querySelector(".total-price") as HTMLDivElement;
@@ -20,14 +28,9 @@ const handlePayment = (value: string) => {
 }
 
 const addPaymentEvent = () => {
-    const buttonList = document.querySelectorAll(".button--payment");
+    const buttonList = document.querySelector(".button--list");
 
-    buttonList.forEach((button) => {
-        const value = (button as HTMLButtonElement).querySelector(".price").textContent;
-
-        button.addEventListener("click", () => handlePayment(value));
-    });
-
+    if(checkInstance(buttonList, HTMLUListElement)) buttonList.addEventListener("click", handlePayment);
 }
 
 export default addPaymentEvent;
