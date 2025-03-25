dc44a11f4c4a26b5a5175604b2207b3c7624f2f9
sanholl <edgar.jang93@hanmail.net>
2025-03-25 20:55:50 +0900

    feat: 금액 입력창 콤마 표시

--- packages/hansol-vanilla/src/ts/common.ts
+++ packages/hansol-vanilla/src/ts/common.ts
@@ -16,8 +16,9 @@ export const BalaceManager = (() => {
 export function updateDisplay(currentBalance: number) {
   const display = document.querySelector<HTMLDivElement>('.vending-machine-display');
   if(!display) throw new Error('자판기 디스플레이를 찾을 수 없습니다.');
-
-  display.textContent = currentBalance.toLocaleString();
+  
+  const formatted = currentBalance.toLocaleString();
+  display.textContent = formatted;
 }
 
 export function addLogMessage(message: string) {
@@ -27,4 +28,10 @@ export function addLogMessage(message: string) {
   
   li.innerHTML = `<span class="log-message">${ message }</span>`;
   logList.appendChild(li);
+}
+
+export function getInputNumberValue(input: HTMLInputElement | null): number {
+  if (!input) return NaN;
+  const raw = input.value.replace(/,/g, '');
+  return Number(raw);
 }
--- packages/hansol-vanilla/src/ts/init.ts
+++ packages/hansol-vanilla/src/ts/init.ts
@@ -1,12 +1,43 @@
-import { BalaceManager } from "./common";
+import { BalaceManager, getInputNumberValue } from "./common";
 
 const currentBalance = BalaceManager.get();
 const vendingMachineDisplay = document.querySelector<HTMLDivElement>('.vending-machine-display');
+const controlInput = document.querySelector<HTMLInputElement>('.control-input');
 
 function initVendingMachine() {
+  initVendingMachineDisplay();
+  initControlInput();
+}
+
+function initVendingMachineDisplay() {
   if(!vendingMachineDisplay) throw new Error('자판기 금액창을 찾을 수 없습니다.');
-  
+
   vendingMachineDisplay.innerText = currentBalance.toLocaleString();
+}
+
+function initControlInput() {
+  if(!controlInput) throw new Error('금액 입력창을 찾을 수 없습니다.');
+
+  controlInput.addEventListener('keyup', handleInputFormat);
+}
+
+function handleInputFormat(e: KeyboardEvent) {
+  const target = e.target as HTMLInputElement;
+  let value = getInputNumberValue(target);
+
+  if(target.value === '') {
+    target.classList.remove('input-error');
+    return;
+  }
+  
+  if(!value) {
+    alert('양수만 입력해주세요');
+    target.classList.add('input-error');
+    target.value = '';
+  } else {
+    target.classList.remove('input-error');
+    target.value = value.toLocaleString();
+  }
 }
 
 initVendingMachine();
