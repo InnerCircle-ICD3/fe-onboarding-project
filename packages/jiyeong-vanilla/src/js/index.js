9e23c94e52d4d94d3fcd4fb0081201aa30c9f726
gzero1728 <gzero1728@gmail.com>
2025-03-25 22:12:36 +0900

    [FEATURE] : 상품목록 상수화 및 상품목록 동적렌더링으로 변경

--- packages/jiyeong-vanilla/src/js/index.js
+++ packages/jiyeong-vanilla/src/js/index.js
@@ -1,6 +1,31 @@
 import { handleInputCoin, handleInsertButton } from "./control";
 import { store } from "./store";
 import { formatNumber } from "./utils";
+import { PRODUCTS } from "./constants";
+
+const createProductButton = (product) => {
+  const button = document.createElement("button");
+  button.className = "product-btn";
+  button.dataset.productId = product.id;
+
+  const nameText = document.createTextNode(product.name);
+  button.appendChild(nameText);
+
+  const priceSpan = document.createElement("span");
+  priceSpan.textContent = `${formatNumber(product.price)}원`;
+  button.appendChild(priceSpan);
+
+  return button;
+};
+
+const renderProducts = () => {
+  const productsGrid = document.querySelector(".products-grid");
+
+  PRODUCTS.forEach((product) => {
+    const productButton = createProductButton(product);
+    productsGrid.appendChild(productButton);
+  });
+};
 
 const renderUI = (state) => {
   const insertAmount = document.querySelector(".insert-amount");
@@ -11,6 +36,7 @@ const renderUI = (state) => {
 };
 
 const initApp = () => {
+  renderProducts();
   handleInputCoin();
   handleInsertButton();
   store.subscribe(renderUI);
