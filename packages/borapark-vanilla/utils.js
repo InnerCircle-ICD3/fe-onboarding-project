92d2f22b080e7093c153dcabccfee23cd4e365c1
Rachel <borapark13@gmail.com>
2025-03-27 02:17:36 +0900

    refactor: updateAmountDisplay to utils

--- packages/borapark-vanilla/utils.js
+++ packages/borapark-vanilla/utils.js
@@ -14,3 +14,15 @@ export const formatCurrencyAsLocaleString = (currency, locale='ko-KR') => {
   }
   return currency.toLocaleString(locale);
 };
+
+/**
+ * 금액 표시 화면 기능
+ * @param {string} selector
+ * @param {number} amount
+ * @param {boolean} needUnit (optional)
+ */
+export const updateAmountDisplay = (selector, amount, needUnit = false) => {
+  const unit = "원";
+  const amountElement = document.querySelector(selector);
+  amountElement.textContent = `${formatLocaleString(amount)}${needUnit ? unit : ""}`;
+};
