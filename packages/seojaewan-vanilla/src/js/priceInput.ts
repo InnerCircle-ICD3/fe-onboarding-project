b47a6fdbf07e1f56725cf5354dc2dfd41193be9f
Ing-yeo_Developer <52366178+SeoJaeWan@users.noreply.github.com>
2025-03-26 22:14:10 +0900

    feat: 로그 표시 (#58)
    * feat: 레이아웃 수정
    
    * feat: 버튼 리스트 생성 스크립트 수정
    
    * feat: input
    
    * refactor: layout
    
    * feat: parse commas
    
    * save
    
    * refactor: change func naming
    
    * feat: payment
    
    * refactor: 불필요한 코드 삭제
    
    * feat: 로그 화면에 노출 및 스크롤링 구현
    
    * feat: update file name
    
    * feat: update file name
    
    * feat: update file name
    
    * chore: update file name
    
    * feat: 로그 메시지 업데이트
    
    * refactor: 결제 로깅 수정
    
    * feat: 타입추론 수정 및 입력 추가
    
    * feat: form 타입 변경
    
    * feat: 타입 수정
    
    * feat: commas 한국으로 고정
    
    * feat: 레이아웃 수정
    
    * feat: update total price
    
    * feat: update total price
    
    * feat: payment 이벤트 위임
    
    * feat: 로그 업데이트

--- packages/seojaewan-vanilla/src/js/priceInput.ts
+++ packages/seojaewan-vanilla/src/js/priceInput.ts
@@ -1,5 +1,6 @@
 import checkInstance from "./checkType";
 import { numberWithComma } from "./commas";
+import addLogging from "./logging";
 import { totalPrice } from "./main";
 
 const handlePriceInput = (e:SubmitEvent) => {
@@ -22,10 +23,12 @@ const handlePriceInput = (e:SubmitEvent) => {
   totalPriceElement.textContent = numberWithComma(updatePrice).toString();
   form.reset();
   priceInputElement.select();
+
+  addLogging(`${numberWithComma(price)}원을 투입했습니다.`);
 };
 
 const handleFocusInput = (e: FocusEvent) => {
-  const inputElement = e.target as HTMLInputElement;
+  const inputElement = e.target;
 
   if(checkInstance(inputElement, HTMLInputElement)) inputElement.select();
 }
