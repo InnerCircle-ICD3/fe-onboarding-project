14b77ff408d12595b93503a24b8da34224f70863
SeoJaeWan <sjw7324@igis.co.kr>
2025-03-28 08:24:54 +0900

    refactor: test case 설명 수정

--- packages/seojaewan-react/src/hooks/useTotalPrice/useTotalPrice.test.ts
+++ packages/seojaewan-react/src/hooks/useTotalPrice/useTotalPrice.test.ts
@@ -1,30 +1,30 @@
 import useTotalPrice from ".";
-import { act, renderHook } from "@testing-library/react";
+import {act, renderHook} from "@testing-library/react";
 
 describe("useTotalPrice 테스트", () => {
-    it("Total Price 값 출력", () => {
+    it("초기 totalPrice 값인 0이 반환된다.", () => {
         const {result} = renderHook(() => useTotalPrice());
 
         expect(result.current.totalPrice).toBe(0);
     });
 
-    it("Total Price 값 증가: 1000", () => {
+    it("increaseTotalPrice에 전달된 1000만큼 totalPrice 값이 증가한다.", () => {
         const {result} = renderHook(() => useTotalPrice());
 
-        act(() => result.current.increaseTotalPrice(1000))
+        act(() => result.current.increaseTotalPrice(1000));
 
         expect(result.current.totalPrice).toBe(1000);
     });
 
-    it("Total Price 값 감소: 500", () => {
+    it("decreaseTotalPrice에 전달된 500만큼 totalPrice 값이 감소한다.", () => {
         const {result} = renderHook(() => useTotalPrice());
 
-        act(() => result.current.decreaseTotalPrice(500))
+        act(() => result.current.decreaseTotalPrice(500));
 
         expect(result.current.totalPrice).toBe(-500);
     });
 
-    it("Total Price 값 초기화", () => {
+    it("increaseTotalPrice로 1000 증가한 totalPrice를 resetTotalPrice로 0으로 초기화된다.", () => {
         const {result} = renderHook(() => useTotalPrice());
 
         act(() => result.current.increaseTotalPrice(1000));
