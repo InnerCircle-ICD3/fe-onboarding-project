8dc9a0209201e3374d99a3656190111367c76bee
hbs9312 <hbs30832@naver.com>
2025-03-27 21:06:30 +0900

    숫자 콤마처리 함수 util 함수로 분리

--- packages/seok-vanilla/src/input-amount.ts
+++ packages/seok-vanilla/src/input-amount.ts
@@ -1,4 +1,5 @@
 import { increase, reset } from "./amount";
+import { formatNumberWithCommas } from "./utills/format";
 
 const $inputAmount = document.querySelector<HTMLInputElement>("#input-amount");
 const $btnInsert = document.querySelector<HTMLButtonElement>("#btn-insert");
@@ -30,7 +31,7 @@ $inputAmount?.addEventListener("beforeinput", (e) => {
 // 입력 값을 숫자로 변환하고 쉼표를 추가
 $inputAmount?.addEventListener("input", (e) => {
   const value = inputToNumber($inputAmount.value);
-  $inputAmount.value = value?.toLocaleString() ?? "";
+  $inputAmount.value = formatNumberWithCommas(value);
 });
 
 function inputToNumber(value: string) {
--- packages/seok-vanilla/src/main.ts
+++ packages/seok-vanilla/src/main.ts
@@ -1,11 +1,14 @@
 import { decrease, increase } from "./amount";
 import db from "./db.json";
 import "./input-amount";
+import { formatNumberWithCommas } from "./utills/format";
 
 const productBtns = db.products.map((product) => {
   const li = document.createElement("li");
   const button = document.createElement("button");
-  button.innerHTML = `${product.name}<br/>${product.price.toLocaleString()}`;
+  button.innerHTML = `${product.name}<br/>${formatNumberWithCommas(
+    product.price
+  )}`;
   button.classList.add("btn-product");
   li.append(button);
   return li;
