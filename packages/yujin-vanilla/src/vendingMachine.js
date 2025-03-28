4f44dacd42fbdc8eaa0436756bfa2d7f1ce38b2a
이유진 <yjlee@fimasia.co.kr>
2025-03-29 00:44:02 +0900

    feat: 구매 기능

--- packages/yujin-vanilla/src/vendingMachine.js
+++ packages/yujin-vanilla/src/vendingMachine.js
@@ -19,6 +19,7 @@ const products = [
 const numberInput = document.querySelectorAll(".number-input");
 const buttonsContainer = document.querySelector(".buttons");
 let total = 0;
+let previousAmount = 0;
 
 buttonsContainer.addEventListener("click", (event) => {
   if (event.target.id === "button-insert") {
@@ -42,7 +43,47 @@ export const renderProducts = () => {
       }
     })
     .join("");
+
+    document.querySelectorAll(".product").forEach((productElement, index) => {
+        productElement.addEventListener("mousedown", () =>
+          handleMouseDown(index)
+        );
+        productElement.addEventListener("mouseup", () => handlePurchase(index));
+    });
 }
+
+const getSelectedProduct = (index) => products[index] || null;
+
+const toggleInputColor = (isHighlight) => {
+  numberInput[0].style.color = isHighlight ? "orange" : "black";
+};
+
+const handleMouseDown = (index) => {
+  const selectedProduct = getSelectedProduct(index);
+  if (!selectedProduct) return;
+
+  previousAmount = getNumericValue(numberInput[0]); // 현재 잔액 저장
+
+  if (selectedProduct.price > total) {
+    numberInput[0].value = setCurrencyToWon(selectedProduct.price);
+    toggleInputColor(true);
+  }
+};
+
+const handlePurchase = (index) => {
+  const selectedProduct = getSelectedProduct(index);
+  if (!selectedProduct) return;
+
+  if (selectedProduct.price > total) {
+    numberInput[0].value = setCurrencyToWon(previousAmount);
+  } else {
+    addLog(`${selectedProduct.name}을(를) 구매했습니다.`);
+    total -= selectedProduct.price;
+    numberInput[0].value = setCurrencyToWon(total);
+  }
+
+  toggleInputColor(false);
+};
 
 const handleInsert = () => {
     const depositAmount = getNumericValue(numberInput[1]);
