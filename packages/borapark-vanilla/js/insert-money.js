f68fb4b1ff8c47285dfe1e0726aa5600cdd292c5
Rachel <borapark13@gmail.com>
2025-03-29 06:46:21 +0900

    wip: insert money

--- packages/borapark-vanilla/js/insert-money.js
+++ packages/borapark-vanilla/js/insert-money.js
@@ -1,24 +1,28 @@
-// #region 투입 기능
+import {updateAmountDisplay, sleep} from "./utils.js";
+
+const $form = document.getElementsByTagName("form")[0];
+const $userAmount = document.getElementById("user-amount");
+
 const handleChangeAmount = (event) => {
-  // 입력창에 focus 했을 때 값 초기화
-  if(event.target.focus) {
-    event.target.value = "";
-  }
-  const userAmount = getUserAmount();
-  currentAmount = Number(userAmount);
-  console.log(currentAmount);
+  const userAmount = event.target.value;
+  setCurrentAmount(parseInt(userAmount));
 };
 
-const handleClickInsertAmountButton = () => {
-  totalAmount += currentAmount;
-  console.log(totalAmount, "total");
+const resetAmount = () => {
+  setCurrentAmount(0);
+  updateAmountDisplay("#user-amount", currentAmount);
+  console.log(currentAmount, "<<<<?")
+};
+
+const handleSubmitInsertAmount = (event) => {
+  event.preventDefault();
+  setTotalAmount();
   updateAmountDisplay(".vending-machine-total-amount", totalAmount);
-  currentAmount = 0; // 초기화
+  resetAmount();
 };
 
 // 투입 금액 입력 이벤트 리스너
-document.getElementById("user-amount").addEventListener("change", handleChangeAmount);
+$userAmount.addEventListener("change", handleChangeAmount);
 
-// 투입 금액 입력 버튼 클릭 이벤트 리스너
-// const insertButton = document.querySelector(".vending-machine-button-fieldset > button:nth-child(1)");
-// insertButton.addEventListener("click", handleClickInsertAmountButton);
+// 투입 금액 입력 폼
+$form.addEventListener("submit", handleSubmitInsertAmount);
