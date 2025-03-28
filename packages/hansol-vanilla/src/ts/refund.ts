2a1cc3f5cbb715b6b5a093c0946812a3ef043c2e
sanholl <edgar.jang93@hanmail.net>
2025-03-28 19:07:41 +0900

    refactor: store분리, 필수 DOM 요소 유효성 검사용 유틸 함수 생성 및 적용

--- packages/hansol-vanilla/src/ts/refund.ts
+++ packages/hansol-vanilla/src/ts/refund.ts
@@ -1,14 +1,23 @@
-import { addLogMessage, BalanceManager, updateDisplay } from "./common";
-
-const refundButton = document.querySelector<HTMLButtonElement>('.refund-button');
+import { addLogMessage, getRequiredElement, updateDisplay } from "./common";
+import { BalanceManager } from "./store";
 
 function handleRefundBalance() {
+  const controlInput = getRequiredElement<HTMLInputElement>('.control-input');
+
+  if(!controlInput.value) {
+    return;
+  }
+
   let currentBalance = BalanceManager.get();
   addLogMessage(`${currentBalance} 원을 반환합니다.`);
   updateDisplay(0);
   BalanceManager.reset();
 }
 
-if(!refundButton) throw new Error('반환 버튼을 찾을 수 없습니다.');
+function initRefundEventListeners() {
+  const refundButton = getRequiredElement<HTMLButtonElement>('.refund-button');
 
-refundButton.addEventListener('click', handleRefundBalance);
+  refundButton.addEventListener('click', handleRefundBalance);
+}
+
+initRefundEventListeners();
