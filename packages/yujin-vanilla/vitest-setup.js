9465122dfb288ecf223a8e8f581f34bab3b0e34f
이유진 <yjlee@fimasia.co.kr>
2025-03-29 00:04:25 +0900

    fix: 코드리뷰 반영

--- packages/yujin-vanilla/vitest-setup.js
+++ packages/yujin-vanilla/vitest-setup.js
@@ -1,6 +1,32 @@
 import { beforeEach } from "vitest";
 
-// 테스트 실행 전에 document를 초기화
 beforeEach(() => {
-  document.body.innerHTML = `<div id='app'></div>`;
+  document.body.innerHTML = `<main class="vending-machine-container">
+        <!-- 자판기 본체 -->
+        <section class="vending-machine">
+            <div class="screen">
+                <input type="text" name="" class="number-input" placeholder="0원" readonly>
+            </div>
+            <div class="products">
+            </div>
+        </section>
+
+        <!-- 조작 패널 -->
+        <section class="control-panel">
+            <div class="money-input">
+                <div class="screen">
+                    <input type="text" name="" class="number-input" placeholder="0원">
+                </div>
+                <div class="buttons">
+                    <button id="button-insert" class="button-blue">투입</button>
+                    <button id="button-refund" class="button-grey">반환</button>
+                </div>
+            </div>
+            <div class="log">
+                <ul class="log-list">
+                    <li>금액을 입력후 투입버튼을 누르세요.</li>
+                </ul>
+            </div>
+        </section>
+    </main>`;
 });
