a1fa57983c26c005c3598ba91e9f31649529c9e2
BORA <a52447879@gmail.com>
2025-03-29 11:52:50 +0900

    feat: 상품 데이터에 disabled 속성 추가

--- packages/bora-vanilla/src/db/productsData.js
+++ packages/bora-vanilla/src/db/productsData.js
@@ -3,46 +3,67 @@ const productsData = [
     id: 1,
     name: '콜라',
     price: 1500,
+    disabled: false,
   },
   {
     id: 2,
     name: '속이사이다',
     price: 1700,
+    disabled: false,
   },
   {
     id: 3,
     name: '판타지판타',
     price: 1500,
+    disabled: false,
   },
   {
     id: 4,
     name: '오뎅국물',
     price: 1800,
+    disabled: false,
   },
   {
     id: 5,
     name: '부장라떼',
     price: 800,
+    disabled: false,
   },
   {
     id: 6,
     name: '판타지판타',
     price: 1500,
+    disabled: false,
   },
   {
     id: 7,
     name: '레드뿔',
     price: 2500,
+    disabled: false,
   },
   {
     id: 8,
     name: '핫세븐',
     price: 1900,
+    disabled: false,
   },
   {
     id: 9,
     name: '커피우유',
     price: 1400,
+    disabled: false,
+  },
+  {
+    id: 10,
+    name: '핫세븐',
+    price: 1900,
+    disabled: true,
+  },
+  {
+    id: 11,
+    name: '커피우유',
+    price: 1400,
+    disabled: true,
   },
 ];
 
