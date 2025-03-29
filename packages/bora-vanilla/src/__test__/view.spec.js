6feef58e161f23eb572167335920501dc08c82a3
BORA <a52447879@gmail.com>
2025-03-29 17:17:13 +0900

    feat: 상품 렌더링 메서드에 대한 테스트 추가

--- packages/bora-vanilla/src/__test__/view.spec.js
+++ packages/bora-vanilla/src/__test__/view.spec.js
@@ -38,8 +38,36 @@ describe('VendingMachineView', () => {
   });
 
   // 렌더링
+  describe('renderProducts', () => {
+    it('상품 데이터를 기반으로 버튼을 렌더링해야 한다.', () => {
+      const products = [
+        { id: 1, name: '콜라', price: 1000 },
+        { id: 2, name: '사이다', price: 1500 },
+      ];
+
+      view.renderProducts(products);
+
+      expect(mockProductButtonContainer.children[0].dataset.id).toBe('1');
+      expect(mockProductButtonContainer.children[0].textContent).toContain('콜라');
+      expect(mockProductButtonContainer.children[0].textContent).toContain('1000원');
+
+      expect(mockProductButtonContainer.children[1].dataset.id).toBe('2');
+      expect(mockProductButtonContainer.children[1].textContent).toContain('사이다');
+      expect(mockProductButtonContainer.children[1].textContent).toContain('1500원');
+    });
+
+    it('제품 갯수에 따라 빈 셀이 유동적으로 렌더링되어야 한다.', () => {
+      const products1 = Array.from({ length: 10 }, (_, i) => ({
+        id: i + 1,
+        name: '상품',
+        price: 1000,
+      }));
+
+      view.renderProducts(products1);
+
+      expect(mockProductButtonContainer.children.length).toBe(12);
+    });
+  });
 
   // 이벤트 핸들러
-
-  it('');
 });
