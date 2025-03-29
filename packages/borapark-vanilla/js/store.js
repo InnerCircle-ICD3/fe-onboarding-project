79dad545157e5d3a33bafd184f480b529b88c6e6
Rachel <borapark13@gmail.com>
2025-03-29 16:28:13 +0900

    refactor: js file separation

---
+++ packages/borapark-vanilla/js/store.js
@@ -0,0 +1,15 @@
+// 상태 변수
+let totalAmount = 0;
+let currentAmount = 0;
+
+// 상태 업데이트 함수
+const setTotalAmount = () => {
+  totalAmount += currentAmount;
+  return totalAmount;
+}
+const setCurrentAmount = (amount) => {
+  currentAmount = amount;
+  return currentAmount;
+}
+
+
