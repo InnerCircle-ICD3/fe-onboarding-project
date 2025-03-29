bdd09c3409ad5f3957ed59022616fd9c49833de9
sanholl <edgar.jang93@hanmail.net>
2025-03-29 14:33:36 +0900

    feat: 상품 버튼 동적 생성

--- packages/hansol-vanilla/src/ts/init.ts
+++ packages/hansol-vanilla/src/ts/init.ts
@@ -1,13 +1,29 @@
 import { formatCurrencyKRW, getRequiredElement } from "./common";
-import { BalanceManager } from "./store";
+import { BalanceManager, products } from "./store";
+
+const currentBalance = BalanceManager.get();
+const productGrid = getRequiredElement<HTMLUListElement>('.product-grid');
+const productTemplate = getRequiredElement<HTMLTemplateElement>('.product-template');
 
 function initVendingMachine() {
   initVendingMachineDisplay();
+  drawProductButton();
+}
+
+function drawProductButton() {
+  for (const product of products) {
+    const buttonClone = productTemplate.content.cloneNode(true) as DocumentFragment;
+    const buttonName = buttonClone.querySelector<HTMLSpanElement>('.product-name');
+    const buttonPrice = buttonClone.querySelector<HTMLSpanElement>('.product-price');
+
+    if(buttonName) buttonName.textContent = product.name;
+    if(buttonPrice) buttonPrice.textContent = product.price;
+    productGrid.appendChild(buttonClone);
+  }
 }
 
 function initVendingMachineDisplay() {
   const vendingMachineDisplay = getRequiredElement<HTMLDivElement>('.vending-machine-display');
-  const currentBalance = BalanceManager.get();
 
   vendingMachineDisplay.innerText = formatCurrencyKRW(currentBalance);
 }
--- packages/hansol-vanilla/src/ts/store.ts
+++ packages/hansol-vanilla/src/ts/store.ts
@@ -1,3 +1,17 @@
+import { Product } from "../types/types";
+
+export const products: Product[] = [
+  { name: '콜라', price: '1500원' },
+  { name: '속이사이다', price: '1700원' },
+  { name: '판타지판타', price: '1500원' },
+  { name: '오뎅국물', price: '1800원' },
+  { name: '부장라떼', price: '800원' },
+  { name: '판타지판타', price: '1500원' },
+  { name: '레드불', price: '2500원' },
+  { name: '핫세븐', price: '1900원' },
+  { name: '커피우유', price: '1400원' }
+];
+
 export const BalanceManager = (() => {
   let balance = 0;
 
