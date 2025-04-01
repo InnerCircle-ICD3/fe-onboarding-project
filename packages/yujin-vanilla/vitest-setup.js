7c6121ce4c95d78e2331b0b21d1cef5b4b384a34
이유진 <yjlee@fimasia.co.kr>
2025-04-01 22:04:27 +0900

    feat: 테스트코드 html 하드코딩에서 파일불러오기

--- packages/yujin-vanilla/vitest-setup.js
+++ packages/yujin-vanilla/vitest-setup.js
@@ -1,32 +1,8 @@
 import { beforeEach } from "vitest";
+import fs from "fs";
+import path from "path";
+const html = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
 
 beforeEach(() => {
-  document.body.innerHTML = `<main class="vending-machine-container">
-        <!-- 자판기 본체 -->
-        <section class="vending-machine">
-            <div class="screen">
-                <input type="text" name="" class="number-input" value="0" readonly>
-            </div>
-            <div class="products">
-            </div>
-        </section>
-
-        <!-- 조작 패널 -->
-        <section class="control-panel">
-            <div class="money-input">
-                <div class="screen">
-                    <input type="text" name="" class="number-input" value="0">
-                </div>
-                <div class="buttons">
-                    <button id="button-insert" class="button-blue">투입</button>
-                    <button id="button-refund" class="button-grey">반환</button>
-                </div>
-            </div>
-            <div class="log">
-                <ul class="log-list">
-                    <li>금액을 입력후 투입버튼을 누르세요.</li>
-                </ul>
-            </div>
-        </section>
-    </main>`;
+    document.body.innerHTML = html.toString()
 });
