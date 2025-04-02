cada1c8df1b8a4612d569e224d40b28c4fc14e6d
hbs9312 <hbs30832@naver.com>
2025-04-02 20:17:37 +0900

    상품 구매 버튼 3열에 맞춰 빈버튼 처리

--- packages/seok-vanilla/src/features/purchase.ts
+++ packages/seok-vanilla/src/features/purchase.ts
@@ -1,23 +1,49 @@
 import db from "../db.json";
 import { NotEnoughtMoneyError } from "../erros/NotEnoughtMoney.error";
+import { Product } from "../model/product";
 import { formatNumberWithCommas } from "../utills/format";
 import { decrease, getAmount } from "./amount";
 import { addLog } from "./print-logs";
 
 const $productList = document.querySelector<HTMLUListElement>("#product-list");
 
+const COLUMN_COUNT = 3;
+const ROW_COUNT = Math.ceil(db.products.length / 3);
+
 function renderProductList() {
-  db.products.map((product) => {
-    const li = document.createElement("li");
-    const button = document.createElement("button");
+  // db.products.forEach((product) => {
+  //   // const li = document.createElement("li");
+  //   // const button = document.createElement("button");
+  //   // button.innerHTML = `${product.name}<br/>${formatNumberWithCommas(
+  //   //   product.price
+  //   // )}`;
+  //   // button.classList.add("btn-product", "btn-color");
+  //   // button.dataset.productId = product.id.toString();
+  //   // li.append(button);
+  //   // $productList?.append(li);
+  //   renderBtnProduct(product);
+  // });
+  // const emptyCount = 3 - (db.products.length % 3);
+
+  for (let i = 0; i < COLUMN_COUNT * ROW_COUNT; i++) {
+    const product = db.products[i];
+    renderBtnProduct(product);
+  }
+}
+
+function renderBtnProduct(product?: Product) {
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  button.classList.add("btn-product", "btn-color");
+  if (product) {
+    button.dataset.productId = product?.id.toString() ?? "";
     button.innerHTML = `${product.name}<br/>${formatNumberWithCommas(
       product.price
     )}`;
-    button.classList.add("btn-product", "btn-color");
-    button.dataset.productId = product.id.toString();
-    li.append(button);
-    $productList?.append(li);
-  });
+  }
+  button.disabled = !product;
+  li.append(button);
+  $productList?.append(li);
 }
 
 $productList?.addEventListener("mousedown", (e) => {
