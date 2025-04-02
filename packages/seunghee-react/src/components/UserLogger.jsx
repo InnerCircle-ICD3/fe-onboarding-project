20b510c12122efecd4b30355ac25379207ee23e5
unknown <tmdgml11342@gmail.com>
2025-04-02 20:37:50 +0900

    feat: scroll update 기능 구현

--- packages/seunghee-react/src/components/UserLogger.jsx
+++ packages/seunghee-react/src/components/UserLogger.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useRef } from "react";
+
 function UserLogger({ logs }) {
+  const logRef = useRef(null);
+
+  useEffect(() => {
+    if (logRef.current) {
+      logRef.current.scrollTop = logRef.current.scrollHeight;
+    }
+  }, [logs]);
+
   return (
-    <div className="user-logger">
+    <div ref={logRef} className="user-logger">
       {logs.map((log, index) => (
         <div key={index}>{log}</div>
       ))}
