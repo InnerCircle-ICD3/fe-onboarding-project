557fdb91f4ed7fa07d06c75dd8024688fa1117c8
sanholl <edgar.jang93@hanmail.net>
2025-04-02 20:33:25 +0900

    feat: test 초기 셋팅

--- packages/hansol-react/vite.config.ts
+++ packages/hansol-react/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+const vitestConfig = {
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    tsconfig: './tsconfig.vitest.json'
+  }
+}
+
 export default defineConfig({
   plugins: [react()],
-})
+  ...vitestConfig
+});
