9e2e16a29773672f7767386e9ce4ed61090cead2
ParkBible <p8yft8903@gmail.com>
2025-04-02 21:39:38 +0900

    refactor: 의미 없는 변수 삭제

--- packages/seongkyeong-vanilla/ts/log.ts
+++ packages/seongkyeong-vanilla/ts/log.ts
@@ -19,21 +19,14 @@ window.addEventListener("logEvent", (e: CustomEventInit<LogInfo>) => {
 });
 
 const createLog = (logInfo: LogInfo) => {
-    let log = "";
-
     switch (logInfo.type) {
         case LogType.CASH_PUT:
-            log = `${logInfo.amount}원을 투입했습니다.`;
-            break;
+            return `${logInfo.amount}원을 투입했습니다.`;
         case LogType.CASH_RETURN:
-            log = `${logInfo.amount}원을 반환합니다.`;
-            break;
+            return `${logInfo.amount}원을 반환합니다.`;
         case LogType.BUY:
-            log = `${logInfo.menuName}을(를) 구매했습니다.`;
-            break;
+            return `${logInfo.menuName}을(를) 구매했습니다.`;
     }
-
-    return log;
 }
 
 const writeLog = (line: string) => {
