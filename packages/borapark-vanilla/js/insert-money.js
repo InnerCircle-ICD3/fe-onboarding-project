471c8b0c8abddc877ef234900f4b49ac9c378bde
Rachel <borapark13@gmail.com>
2025-04-02 19:06:57 +0900

    feat: add insert money in index.js

--- packages/borapark-vanilla/js/insert-money.js
+++ packages/borapark-vanilla/js/insert-money.js
@@ -1,28 +1,21 @@
 import { updateAmountDisplay } from "./utils.js";
 import { setCurrentAmount, setTotalAmount } from "./store.js";
 
-const $form = document.getElementsByTagName("form")[0];
-const $userAmount = document.getElementById("user-amount");
+export const $form = document.getElementsByTagName("form")[0];
+export const $userAmount = document.getElementById("user-amount");
 
-const handleChangeAmount = (event) => {
+export const handleChangeAmount = (event) => {
   const userAmount = event.target.value;
   setCurrentAmount(parseInt(userAmount));
 };
 
 const resetAmount = () => {
   $form.reset();
-  updateAmountDisplay("#user-amount", currentAmount);
 };
 
-const handleSubmitInsertAmount = (event) => {
+export const handleSubmitInsertAmount = (event) => {
   event.preventDefault();
-  setTotalAmount();
+  const totalAmount = setTotalAmount();
   updateAmountDisplay(".vending-machine-total-amount", totalAmount);
   resetAmount();
 };
-
-// 투입 금액 입력 이벤트 리스너
-$userAmount.addEventListener("change", handleChangeAmount);
-
-// 투입 금액 입력 폼
-$form.addEventListener("submit", handleSubmitInsertAmount);
