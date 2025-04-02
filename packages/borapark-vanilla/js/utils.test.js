c64e104b32cea213eaf3b09564cc5913d3b63038
Rachel <borapark13@gmail.com>
2025-04-02 17:32:07 +0900

    fix: test code

--- packages/borapark-vanilla/js/utils.test.js
+++ packages/borapark-vanilla/js/utils.test.js
@@ -41,47 +41,51 @@ describe("formatCurrencyAsLocaleString()", () => {
   });
 });
 
+/**
+ * @jest-environment jsdom
+ */
 describe("updateAmountDisplay", () => {
   let $element;
 
   beforeEach(() => {
     document.body.innerHTML = '<div id="amount-display"></div>';
     $element = document.querySelector("#amount-display");
+    expect($element).not.toBeNull();
   });
 
   test("숫자를 세 자리마다 쉼표(,)를 추가한 문자열로 표시한다.", () => {
-    updateAmountDisplay("#amount-display", 1_000_000);
+    updateAmountDisplay($element, 1_000_000);
     expect($element.textContent).toBe("1,000,000");
   });
 
   test("단위를 포함하는 경우, 단위를 추가한 문자열을 표시한다.", () => {
-    updateAmountDisplay("#amount-display", 1_000_000, true);
+    updateAmountDisplay($element, 1_000_000, true);
     expect($element.textContent).toBe("1,000,000원");
   });
 
   test("0 값을 전달하면 '0'이 표시된다.", () => {
-    updateAmountDisplay("#amount-display", 0);
+    updateAmountDisplay($element, 0);
     expect($element.textContent).toBe("0");
   });
 
   test("유효하지 않은 selector를 전달하면 오류가 발생하지 않는다.", () => {
-    expect(() => updateAmountDisplay("#invalid-selector", 1_000)).not.toThrow();
+    expect(() => updateAmountDisplay($element, 1_000)).not.toThrow();
   });
 
   test("음수 값을 전달하면 에러가 발생한다.", () => {
-    expect(() => updateAmountDisplay("#amount-display", -500_000)).toThrow(
+    expect(() => updateAmountDisplay($element, -500_000)).toThrow(
       "Invalid currency: only non-negative integers are allowed"
     );
   });
 
   test("소수 값을 전달하면 에러가 발생한다.", () => {
-    expect(() => updateAmountDisplay("#amount-display", 1_000.5)).toThrow(
+    expect(() => updateAmountDisplay($element, 1_000.5)).toThrow(
       "Invalid currency: only non-negative integers are allowed"
     );
   });
 
   test("숫자가 아닌 값을 전달하면 에러가 발생한다.", () => {
-    expect(() => updateAmountDisplay("#amount-display", "1_000")).toThrow(
+    expect(() => updateAmountDisplay($element, "1_000")).toThrow(
       "Invalid currency: only non-negative integers are allowed"
     );
   });
