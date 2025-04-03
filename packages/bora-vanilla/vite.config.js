3e6c9ce11a5f35012f39d16d7511a43b31c3e315
BORA <a52447879@gmail.com>
2025-04-03 20:30:35 +0900

    feat: vitest 및 테스트 설정 추가

--- packages/bora-vanilla/vite.config.js
+++ packages/bora-vanilla/vite.config.js
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
-  publicDir: '../public',
   build: {
     outDir: '../dist',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['__test__/*.spec.js?(x)'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: ['db/productsData.js', 'js/dom-selectors.js', 'js/main.js'],
+    },
   },
 });
