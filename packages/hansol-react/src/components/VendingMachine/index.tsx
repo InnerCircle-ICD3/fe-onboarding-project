74d497429d3cb659cc8935e485ea2dd6ce5b300c
sanholl <edgar.jang93@hanmail.net>
2025-04-03 20:16:44 +0900

    feat: 상품 버튼 동적 구현

--- packages/hansol-react/src/components/VendingMachine/index.tsx
+++ packages/hansol-react/src/components/VendingMachine/index.tsx
@@ -1,16 +1,39 @@
 import {
   ProductGrid,
   VendingMachineContainer,
-  VendingMachineDisplay,  
+  VendingMachineDisplay,
+  ProductButton,
+  ProductName,
+  ProductPrice,
 } from "./VendingMachine.styles";
+import { products } from "../../data/products";
+import { formatCurrencyKRW } from "../../utils/common";
 
 const VendingMachine = () => {
+  const PRODUCT_COLUMNS = 3;
+  const PRODUCT_ROWS = Math.ceil(products.length / PRODUCT_COLUMNS);
+  const TOTAL_SLOT = PRODUCT_COLUMNS * PRODUCT_ROWS;
+
+  const slots = Array.from({ length: TOTAL_SLOT }, (_, index) => {
+    const product = products[index];
+
+    if (product) {
+      return (
+        <ProductButton key={index}>
+          <ProductName>{product.name}</ProductName>
+          <ProductPrice>{formatCurrencyKRW(Number(product.price.replace(/원/, '')))}원</ProductPrice>
+        </ProductButton>
+      );
+    } else {
+      return <ProductButton key={index} disabled />;
+    }
+  });
 
   return (
     <VendingMachineContainer>
       <VendingMachineDisplay>{/* 잔액 표시용 상태값이 들어갈 예정 */}</VendingMachineDisplay>
       <ProductGrid>
-        
+        {slots}
       </ProductGrid>
     </VendingMachineContainer>
   );
