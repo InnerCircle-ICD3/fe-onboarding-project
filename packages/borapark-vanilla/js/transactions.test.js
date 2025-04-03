d61cdc5b6af612e8f3b9e7b3e1bf7e1577985ae5
Rachel <borapark13@gmail.com>
2025-04-03 11:53:05 +0900

    test: return Amount test add

--- packages/borapark-vanilla/js/transactions.test.js
+++ packages/borapark-vanilla/js/transactions.test.js
@@ -1,7 +1,7 @@
 import { totalAmount, currentAmount, setCurrentAmount } from "./store.js";
-import { handleChangeAmount, handleSubmitInsertAmount } from "./insert-money.js";
+import { handleChangeAmount, handleSubmitInsertAmount, handleReturnMoney } from "./transactions.js";
 
-describe("투입 기능 테스트", () => {
+describe("투입/반환 기능 테스트", () => {
   let $form;
   let $userAmount;
   let $vendingMachineTotalAmount;
@@ -30,25 +30,25 @@ describe("투입 기능 테스트", () => {
     setCurrentAmount(0);
   });
 
-  test("투입 금액을 유저가 input 창에 입력하면, 현재 금액이 업데이트 된다.", () => {
+  test("투입 금액을 유저가 input 창에 입력하면, 현재 금액이 업데이트 된다. - handleChangeAmount()", () => {
     const userAmount = 1000;
     handleChangeAmount({ target: { value: userAmount } });
     expect(currentAmount).toBe(userAmount);
   });
 
-  test("숫자가 아닌 값이 입력되면 현재 금액이 0으로 설정된다.", () => {
+  test("숫자가 아닌 값이 입력되면 현재 금액이 0으로 설정된다. - handleChangeAmount()", () => {
     handleChangeAmount({ target: { value: "abc" } });
     expect(currentAmount).toBe(0);
   });
 
-  test("현재 금액이 업데이트 되면, 현재 금액의 누적 금액이 총 금액으로 업데이트 된다.", () => {
+  test("현재 금액이 업데이트 되면, 현재 금액의 누적 금액이 총 금액으로 업데이트 된다. - handleSubmitInsertAmount()", () => {
     const userAmount = 1000;
     handleChangeAmount({ target: { value: userAmount } });
     handleSubmitInsertAmount(new Event("submit"));
     expect(totalAmount).toBe(userAmount);
   });
 
-  test("금액 투입 후 form이 reset되고 현재 금액이 0으로 초기화된다.", () => {
+  test("금액 투입 후 form이 reset되고 현재 금액이 0으로 초기화된다. - handleSubmitInsertAmount()", () => {
     // 1. 먼저 금액을 입력하고
     const userAmount = 1000;
     handleChangeAmount({ target: { value: userAmount } });
@@ -60,5 +60,12 @@ describe("투입 기능 테스트", () => {
     expect($userAmount.value).toBe("");
     expect(currentAmount).toBe(0);
     expect(totalAmount).toBe(userAmount); // totalAmount는 누적되어야 함
+  });
+
+  test("반환 버튼을 누르면 현재 금액이 0으로 초기화되고 총 금액이 0으로 설정된다. - handleReturnMoney()", () => {
+    const userAmount = 1000;
+    handleChangeAmount({ target: { value: userAmount } });
+    handleSubmitInsertAmount(new Event("submit"));
+    handleReturnMoney();
   });
 });
