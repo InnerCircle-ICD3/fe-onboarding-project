92d3d9ae830d66750bee9254ed4934f58ec21390
sanholl <edgar.jang93@hanmail.net>
2025-04-05 11:02:36 +0900

    chore: common 테스트 코드 수정

--- packages/hansol-react/src/test/common.test.ts
+++ packages/hansol-react/src/test/common.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest';
 import { getInputNumberValue, formatCurrencyKRW, appendParticle } from '../utils/common';
 
-describe('getInputNumberValue', () => {
+describe('getInputNumberValue()', () => {
   it('쉼표가 있는 숫자 문자열을 숫자로 반환한다', () => {
     const input = document.createElement('input');
     input.value = '1,500';
@@ -13,14 +13,14 @@ describe('getInputNumberValue', () => {
   });
 });
 
-describe('formatCurrencyKRW', () => {
+describe('formatCurrencyKRW()', () => {
   it('숫자를 한국 원화 포맷으로 포맷팅한다', () => {
     expect(formatCurrencyKRW(1000)).toBe('1,000');
     expect(formatCurrencyKRW(2500000)).toBe('2,500,000');
   });
 });
 
-describe('appendParticle', () => {
+describe('appendParticle()', () => {
   it('받침이 있는 단어에는 "을"을 붙인다', () => {
     expect(appendParticle('밥')).toBe('밥을');
     expect(appendParticle('떡')).toBe('떡을');
