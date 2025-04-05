b7dbc5dbc2af6a1b2aabf6f13b4af8e4fed7491c
sanholl <edgar.jang93@hanmail.net>
2025-04-05 10:53:37 +0900

    feat: reducer 테스트 코드 수정

--- packages/hansol-react/src/test/balanceReducer.test.ts
+++ packages/hansol-react/src/test/balanceReducer.test.ts
@@ -1,20 +1,21 @@
 import { describe, expect, it } from 'vitest';
-import { balanceReducer, initialState } from '../store/balance/balanceReducer';
+import { balanceReducer } from '../store/balance/balanceReducer';
 
 
 describe('balanceReducer', () => {
-  it('ADD_BALANCE는 잔액을 더한다', () => {
+  it('ADD_BALANCE 액션이 들어오면 initialState에 payload만큼 더해진 결과가 반환된다.', () => {
+    const initialState = { balance: 1000 };
     const result = balanceReducer(initialState, {
       type: 'ADD_BALANCE',
       payload: 1000,
     });
 
-    expect(result.balance).toBe(1000);
+    expect(result.balance).toBe(2000);
   });
 
-  it('PURCHASE_PRODUCT는 잔액을 감소시킨다', () => {
-    const startState = { balance: 2000 };
-    const result = balanceReducer(startState, {
+  it('PURCHASE_PRODUCT 액션이 들어오면 initialState에서 payload만큼 차감된 결과가 반환된다.', () => {
+    const initialState = { balance: 2000 };
+    const result = balanceReducer(initialState, {
       type: 'PURCHASE_PRODUCT',
       payload: 500,
     });
@@ -22,9 +23,10 @@ describe('balanceReducer', () => {
     expect(result.balance).toBe(1500);
   });
 
+
   it('RETURN_BALANCE는 잔액을 0으로 초기화한다', () => {
-    const startState = { balance: 3000 };
-    const result = balanceReducer(startState, {
+    const initialState = { balance: 3000 };
+    const result = balanceReducer(initialState, {
       type: 'RETURN_BALANCE',
     });
 
--- packages/hansol-react/src/test/logMessageReducer.test.ts
+++ packages/hansol-react/src/test/logMessageReducer.test.ts
@@ -2,13 +2,14 @@ import { describe, expect, it } from "vitest";
 import { initialState, logMessageReducer } from "../store/logMessage/logMessageReducer";
 
 describe('logMessageReducer', () => {
-  it('ADD_LOG_MESSAGE는 로그 메시지를 추가한다', () => {
+  it('ADD_LOG_MESSAGE 액션으로 전달된 message가 logMessages 배열에 추가된다.', () => {
+    const message = '1,000원을 투입했습니다.';
+  
     const result = logMessageReducer(initialState, {
       type: 'ADD_LOG_MESSAGE',
-      payload: '1,000원을 투입했습니다.',
+      payload: message,
     });
-
-    expect(result.logMessages[0]).toBe('1,000원을 투입했습니다.');
-  }
-  );
+  
+    expect(result.logMessages).toContain(message);
+  });
 });
